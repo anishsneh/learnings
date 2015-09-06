2b1316117bd949e347c73d4aa6a0be6ea0aabafb
anishsneh <anishsneh@localhost>
2015-09-06 18:39:09 -0400

    Updated comments for Kafka Producer/Consumer Demo | http://www.anishsneh.com

--- kafka-consumer/src/main/java/com/anishsneh/demo/kafka/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/anishsneh/demo/kafka/KafkaConsumer.java
@@ -11,31 +11,41 @@ import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
 
+/**
+ * The Class KafkaConsumer.
+ * 
+ * @author Anish Sneh
+ */
 public class KafkaConsumer {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 
-		Properties props = new Properties();
+		final Properties props = new Properties();
 		props.put("zookeeper.connect", "localhost:2181");
 		props.put("group.id", "first_consumer_group");
 		props.put("zookeeper.session.timeout.ms", "30000");
 		props.put("zookeeper.sync.time.ms", "200");
 		props.put("auto.commit.interval.ms", "1000");
 
-		ConsumerConfig cf = new ConsumerConfig(props);
+		final ConsumerConfig cf = new ConsumerConfig(props);
 
-		ConsumerConnector consumer = Consumer.createJavaConsumerConnector(cf);
+		final ConsumerConnector consumer = Consumer.createJavaConsumerConnector(cf);
 
-		String topic = "kafka-demo-topic";
+		final String topic = "kafka-demo-topic";
 
-		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		final Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
 		topicCountMap.put(topic, new Integer(1));
-		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-		List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+		final Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+		final List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
 
-		KafkaStream<byte[], byte[]> stream = streams.get(0);
+		final KafkaStream<byte[], byte[]> stream = streams.get(0);
 
-		ConsumerIterator<byte[], byte[]> it = stream.iterator();
+		final ConsumerIterator<byte[], byte[]> it = stream.iterator();
 		int i = 1;
 		while (it.hasNext()) {
 			System.out.println("INDEX: " + i + ": MESSAGE: " + new String(it.next().message()));
--- kafka-producer/src/main/java/com/anishsneh/demo/kafka/KafkaProducer.java
+++ kafka-producer/src/main/java/com/anishsneh/demo/kafka/KafkaProducer.java
@@ -7,26 +7,37 @@ import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
+/**
+ * The Class KafkaProducer.
+ * 
+ * @author Anish Sneh
+ */
 public class KafkaProducer {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws InterruptedException the interrupted exception
+	 */
 	public static void main(String[] args) throws InterruptedException {
 
-		Properties props = new Properties();
+		final Properties props = new Properties();
 
 		props.put("metadata.broker.list", "server01:9092,server02:9092");
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		props.put("request.required.acks", "1");
 
-		ProducerConfig config = new ProducerConfig(props);
+		final ProducerConfig config = new ProducerConfig(props);
 
-		Producer<String, String> producer = new Producer<String, String>(config);
+		final Producer<String, String> producer = new Producer<String, String>(config);
 
-		String topic = "kafka-demo-topic";
+		final String topic = "kafka-demo-topic";
 
 		for (int i = 1; i <= 1000; i++) {
-			String msg = "UUID: " + UUID.randomUUID().toString() + "; AT: " + System.currentTimeMillis();
+			final String msg = "UUID: " + UUID.randomUUID().toString() + "; AT: " + System.currentTimeMillis();
 			System.out.println(msg);
-			KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, String.valueOf(i), msg);
+			final KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, String.valueOf(i), msg);
 			producer.send(data);
 			Thread.sleep(2000L);
 		}
